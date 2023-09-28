7c9396cc8a09edf24babc5c1f9d61acc00fe32a6
Grace Li <133687652+gli-i@users.noreply.github.com>
2023-09-27 22:27:02 -0700

    Let display show the input from clicking buttons

--- calc-js.js
+++ calc-js.js
@@ -27,11 +27,32 @@ function operate(operator, a, b){
 }
 
 /* Main */
-let num_a;
-let num_b;
-let operator;
+const display = document.querySelector('#display-text');
+const butNums = document.querySelectorAll('.butNum');
+const butOps = document.querySelectorAll('.butOp');
+const butNegate = document.querySelector('.butNegate');
+const butClear = document.querySelector('.butClear');
+const butCompute = document.querySelector('.butCompute');
 
-console.log(operate('+',34,1));
-console.log(operate('-',87,103));
-console.log(operate('*',2,4));
-console.log(operate('/',3,7));
+let input_str = "";
+
+butNums.forEach(butNum => butNum.addEventListener('click', e => {
+    display.textContent += e.target.value;
+    input_str = input_str.concat(e.target.value);
+}));
+
+butOps.forEach(butOp => butOp.addEventListener('click', e => {
+    display.textContent += e.target.value;
+    input_str = input_str.concat(`,${e.target.value},`);
+}));
+
+butNegate.addEventListener('click', e => {
+    display.textContent += e.target.value;
+    input_str = input_str.concat(",-");
+});
+
+butClear.addEventListener('click', e => {
+    console.log(input_str);
+    input_str = "";
+    display.textContent = "";
+});
